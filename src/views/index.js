40d2db526393d55def1cbc943cd67ac26374cea8
Juan Raul Martinez Lopez <juggantiss@gmail.com>
2021-04-19 23:16:49 -0500

    Create structure for app

---
+++ src/views/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import Home from "../views/pages/Home";
+
+function index() {
+  return (
+    <>
+      <NavBar />
+      <Home />
+    </>
+  );
+}
+
+export default index;
